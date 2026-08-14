4e7219cb9c5abac8405c015b131e85e1451f1e5c
Konstantin S. <havendv@gmail.com>
2026-08-14 19:39:47 +0400

    feat: Updated OpenAPI spec (#18)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/DoubaoSeed3D/Generated/DoubaoSeed3D.DoubaoSeed3DClient.g.cs
+++ src/libs/DoubaoSeed3D/Generated/DoubaoSeed3D.DoubaoSeed3DClient.g.cs
@@ -37,10 +37,17 @@ namespace DoubaoSeed3D
 
         /// <inheritdoc/>
         public global::DoubaoSeed3D.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::DoubaoSeed3D.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::DoubaoSeed3D.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
